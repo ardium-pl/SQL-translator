8234a13c6e479e52438a5415c4e1c127d89ab221
Szymon <szymonskowronnn@gmail.com>
2024-09-24 15:57:23 +0200

    :<

--- client/SQL-translator/src/app/app.component.ts
+++ client/SQL-translator/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'SQL-translator';
